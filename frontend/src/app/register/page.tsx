a813ecf975aa27337b8b4fc76828c3b9a059edba
Deen7028 <kamaruding7028@gmail.com>
2026-04-16 13:23:21 +0700

    ีu

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import TextField from '@mui/material/TextField';
 import RegisterForm from "../skill/page";
 
 
